8ba47403313ae7b55affc6e19b1b91f281c4859d
Will Sussman <wsussman@mit.edu>
2022-12-05 16:52:56 -0500

    Cleaned up

--- cpu/raw_cpu.sh
+++ cpu/raw_cpu.sh
@@ -1,10 +1,5 @@
 #!/bin/sh
 
-# vmstat 1 | awk '{now=strftime("%Y-%m-%d %T "); print now $0}'
-
-    #           disk0       cpu    load average
-    # KB/t  tps  MB/s  us sy id   1m   5m   15m
-
 dir='./logs'
 
 sampling_interval=1 # sec

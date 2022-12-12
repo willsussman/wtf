0d473b616dc56593d44d6467d5ea897fb21a8fa4
Will Sussman <wsussman@mit.edu>
2022-12-12 13:36:21 -0500

    Small tweaks

--- wifi/raw_wifi.py
+++ wifi/raw_wifi.py
@@ -70,8 +70,12 @@ lastAssocStatus: {self.lastAssocStatus}\n\
 
 def sample_airport():
 	obj = subprocess.run([f'{PATH_TO_AIRPORT}', '-I'], capture_output=True, text=True)
+
+	# os.system('clear')
+	# print(obj.stdout)
+
 	lines = obj.stdout.splitlines()
-	print(lines)
+	# print(lines)
 	return Airport(lines)
 
 def main():

8fbaf73c8a2ece23fe9c637209ca38a5848dc53b
Will Sussman <wsussman@mit.edu>
2022-12-12 16:58:33 -0500

    Raw, not cpu

--- cpu/raw.sh
+++ cpu/raw.sh
@@ -4,4 +4,4 @@ dir='./data'
 sampling_interval=1 # sec
 
 mkdir -p $dir
-iostat -w $sampling_interval | ts "%Y-%m-%d %H:%M:%.S" > $dir/cpu.txt
+iostat -w $sampling_interval | ts "%Y-%m-%d %H:%M:%.S" > $dir/raw.txt

8d8d8205a936297a2b595c5d86fa26459ba2c4f2
Will Sussman <wsussman@mit.edu>
2022-12-16 04:25:58 +0000

    More testing

--- wifi/main.py
+++ wifi/main.py
@@ -27,7 +27,7 @@ def vitals_wifi(t, T):
             date_split, time_split, rssi_split, txrate_split = line.split()
 
             time = datetime.fromisoformat(f'{date_split} {time_split}')
-            print(f'time={time}, time>t={time>t}, t-time>T={t-time>T}')
+            print(f'time={time}, time>t={time>t}, t-time>T={t-time}')
             if time > t:
                 continue
             if t - time > T:
